17c30be51e653782f66800800aa88f2361a4ca2c
Alfiya <alfiya.udc@gmail.com>
2018-04-10 23:15:46 +0700

    Added new dependencies in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,77 @@
 'use strict';
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var run = require("run-sequence");
+
 
 gulp.task("sass", function(){
     gulp.src("sass/style.scss")
+    .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest("css"));
+    .pipe(postcss([
+        autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+        ]}),
+        mqpacker({
+            sort: true
+        })
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
+    //.pipe(server.reload({stream: true}))
+});
+
+gulp.task("images", function() {
+    return gulp.src("img/**/*.{png,jpg,gif}")
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest("img"));
+});
+
+gulp.task("serve", ["sass"], function () {
+    server.init({
+        server: "."
+    });
+
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("*.html")
+        .on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+    run("sass", "images", fn);
+});
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "fonts/**/*.{woff,woff2}",
+        "img/**",
+        "js/**",
+        "*.html"
+    ], {
+        base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+    return del("build");
 });
 
